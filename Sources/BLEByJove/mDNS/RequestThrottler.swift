7be6bddbf9b01047ad91908909176a7a5cc05a92
David Giovannini <dsjove@gmail.com>
2025-05-21 12:25:43 -0500

    add completion to throttle

--- Sources/BLEByJove/mDNS/RequestThrottler.swift
+++ Sources/BLEByJove/mDNS/RequestThrottler.swift
@@ -8,36 +8,58 @@
 import Foundation
 
 public class RequestThrottler {
+	public enum RequestResult {
+		case dropped
+		case failure(Error)
+		case success(Data)
+	}
+
 	private var lastRequestTime: Date?
-	private var pendingRequest: URLRequest?
+	private var pendingRequest: ((URLRequest, ((RequestResult) -> Void)?))?
 	private let queue = DispatchQueue(label: "RequestThrottlerQueue")
 
 	public init() {
 	}
 
-	public func sendRequest(url: URL) {
+	public func sendRequest(url: URL, completion: ((RequestResult) -> Void)? = nil) {
+		sendRequest(request: URLRequest(url: url), completion: completion)
+	}
+
+	public func sendRequest(request: URLRequest, completion: ((RequestResult) -> Void)? = nil) {
 		queue.async {
 			let now = Date()
-			let request = URLRequest(url: url)
 			if let lastTime = self.lastRequestTime, now.timeIntervalSince(lastTime) < 1.0 {
-				self.pendingRequest = request
+				if let pendingRequestCompletion = completion {
+					completion?(.dropped)
+				}
+				self.pendingRequest = (request, completion)
 				return
 			}
 
 			self.lastRequestTime = now
-			self.execute(request)
+			self.execute(request, completion)
 			DispatchQueue.global().asyncAfter(deadline: .now() + 1.0) {
 				self.queue.async {
 					if let pending = self.pendingRequest {
 						self.pendingRequest = nil
-						self.execute(pending)
+						self.execute(pending.0, pending.1)
 					}
 				}
 			}
 		}
 	}
 
-	private func execute(_ request: URLRequest) {
-		URLSession.shared.dataTask(with: request).resume()
+	private func execute(_ request: URLRequest, _ completion: ((RequestResult) -> Void)?) {
+		URLSession.shared.dataTask(with: request) { data, response, error in
+			if let error = error {
+				completion?(.failure(error))
+			}
+			else if let data = data {
+				completion?(.success(data))
+			}
+			else {
+				completion?(.dropped)
+			}
+		}.resume()
 	}
 }
