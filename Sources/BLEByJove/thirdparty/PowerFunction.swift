34a2f10e64d857a4f7e317adfeda220cdf2226a9
David Giovannini <dsjove@mac.com>
2026-01-16 19:12:14 -0600

    add PF mode

--- Sources/BLEByJove/thirdparty/PowerFunction.swift
+++ Sources/BLEByJove/thirdparty/PowerFunction.swift
@@ -15,37 +15,51 @@ public enum PFPort: UInt8, CaseIterable, Hashable, Equatable, BTSerializable {
 	}
 }
 
+public enum PFMode: UInt8, CaseIterable, Hashable, Equatable, BTSerializable {
+	case combo = 0
+	case single = 1
+
+	public init() {
+		self = .combo
+	}
+}
+
 public struct PFCommand: Hashable, Equatable, BTSerializable {
 	public let channel: UInt8 // 1..4
 	public let port: PFPort
 	public let power: UInt8 // 0..15
+	public let mode: PFMode
 
 	public var packedSize: Int {
-		3
+		4
 	}
 
 	public init() {
 		self.channel = 1
 		self.port = .A
 		self.power = 0
+		self.mode = .combo
 	}
 
-	public init(channel: UInt8 = 1, port: PFPort = .A, power: UInt8 = 0) {
+	public init(channel: UInt8 = 1, port: PFPort = .A, power: UInt8 = 0, mode: PFMode = .combo) {
 		self.channel = channel
 		self.port = port
 		self.power = power
+		self.mode = mode
 	}
 
 	public init(unpack data: Data, _ cursor: inout Int) throws {
 		channel = try .init(unpack: data, &cursor)
 		port = try .init(unpack: data, &cursor)
 		power = try .init(unpack: data, &cursor)
+		mode = try .init(unpack: data, &cursor)
 	}
 
 	public func pack(btData data: inout Data) {
 		channel.pack(btData: &data)
 		port.pack(btData: &data)
 		power.pack(btData: &data)
+		mode.pack(btData: &data)
 	}
 }
 
